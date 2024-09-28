2c3637f409c19576b4bd8eb06ff6c6a439f26d22
CalebBunch <rcbcg123@gmail.com>
2024-09-28 09:23:23 -0400

    sort crime data

--- parse_data.py
+++ parse_data.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+CRIME_PATH = "data/crime_data.csv"
+OUTPUT_PATH = "data/cleaned_crime_data.csv"
+
 def parse_csv():
-    df = pd.read_csv("crime_data.csv", low_memory=False)
+    df = pd.read_csv(CRIME_PATH, low_memory=False)
     columns_to_drop = [
         "Report Number", "Report Date", "Location", "Offense Start Date",
         "Offense End Date", "Social Media", "Victim Count", "Day of the week",
@@ -13,9 +16,12 @@ def parse_csv():
 
     df.drop(columns=columns_to_drop, inplace=True, errors="ignore")
     print("Remaining columns:", df.columns.tolist())
+    
+    df.sort_values(by=['Longitude', 'Latitude'], inplace=True)
+    
     print(df.head().to_string())
     unique_nibrs_count = df["NIBRS Code Name"].nunique()
     print(f"Number of unique NIBRS code names: {unique_nibrs_count}")
 
-    df.to_csv("cleaned_crime_data.csv", index=False)
+    df.to_csv(OUTPUT_PATH, index=False)
 
