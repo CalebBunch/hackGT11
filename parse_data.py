631605396e839d30a847f4cebf3d4e26231607ea
CalebBunch <rcbcg123@gmail.com>
2024-09-28 09:46:32 -0400

    sort data

--- parse_data.py
+++ parse_data.py
@@ -1,9 +1,12 @@
+from os import error
 import pandas as pd
 
 CRIME_PATH = "data/crime_data.csv"
-OUTPUT_PATH = "data/cleaned_crime_data.csv"
+CRIME_OUTPUT_PATH = "data/cleaned/cleaned_crime_data.csv"
+ROADS_PATH = "data/pavement_condition_index.csv"
+ROADS_OUTPUT_PATH = "data/cleaned/cleaned_pavement_condition_index.csv"
 
-def parse_csv():
+def parse_crime_csv():
     df = pd.read_csv(CRIME_PATH, low_memory=False)
     columns_to_drop = [
         "Report Number", "Report Date", "Location", "Offense Start Date",
@@ -23,5 +26,22 @@ def parse_csv():
     unique_nibrs_count = df["NIBRS Code Name"].nunique()
     print(f"Number of unique NIBRS code names: {unique_nibrs_count}")
 
-    df.to_csv(OUTPUT_PATH, index=False)
+    df.to_csv(CRIME_OUTPUT_PATH, index=False)
+
+
+def parse_roads_csv():
+    # street name, from street, to street, Pavement Condition Index, Surface Distress Index, Roughness Index
+    # 0-25(very poor), 26-40(poor), 41-50(marginal), 51-60(fair), 61-70(good), 71-85(very good), 86-100(excellent)
+    df = pd.read_csv(ROADS_PATH)
+
+    keep = ["Street Name", "From Street Name", "To Street Name", "Surface Distress Index", "Roughness Index", "Condition Rating"]
+    
+    df_filtered = df[keep]
+
+    print("Remaining columns:", df_filtered.columns.tolist())
+    df_filtered.sort_values(by=["Street Name"], inplace=True)
+    
+    print(df_filtered.head().to_string())
+
+    df_filtered.to_csv(ROADS_OUTPUT_PATH, index=False)
 
