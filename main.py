2dcd2decde0b52084396d3424349b3ff97bacaad
CalebBunch <rcbcg123@gmail.com>
2024-09-27 20:10:34 -0400

    api key

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from . import create_app
 
+# NOTE: this is bad practice
+API_KEY = "sk.eyJ1IjoiY2J1bmNoIiwiYSI6ImNtMWxkdHRhNDA3eWgyam92b3ByMmJzMm0ifQ.2ZWLAmAditf8rkHwAEonWA"
+
 def main() -> None:
     app = create_app()
     app.run()
