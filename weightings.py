528484d7eea51a38bf426b52f0f28b60a9495e89
Caleb Bunch <100161536+CalebBunch@users.noreply.github.com>
2024-09-29 04:56:04 -0400

    Delete weightings.py

--- weightings.py
+++
@@ -1,6 +0,0 @@
-CRIME_WEIGHT = 0.9
-ROAD_WEIGHT = 0.6
-LIGHT_WEIGHT = 0.6
-
-def calculate_weightings(crimes: list[int], roads: list[int], lights: int) -> int:
-    return CRIME_WEIGHT * sum(crimes) + ROAD_WEIGHT * sum([(100 - r) for r in roads]) + LIGHT_WEIGHT * lights
